9d9f625c9c1f927d3c9d8fe74e65ecabcc0fd6c7
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-12-20 16:23:29 +0100

    added vue

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+
+      }
+    },
+    created(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
+            console.log(email.data.response);
+        });
+    },
+}).mount('#app')
